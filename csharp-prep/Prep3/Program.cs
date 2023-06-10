290ea938c41a0a2df84be45203f0c7ded195b4c3
JohanMV1 <vil22018@byui.edu>
2023-06-10 14:11:50 -0700

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,11);
+
+        int userNumber  = -1;
+        Console.WriteLine("Guess a number from 1 to 10");
+
+
+        while (userNumber != number)
+        {
+            
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber == number)
+            {
+                Console.WriteLine("Your number is correct! ");
+            }
+            else if (userNumber <= number)
+            {
+                Console.WriteLine("Try a higher number ");
+
+            }
+            else if( userNumber >= number)
+            {
+                Console.WriteLine("Try a lower number");
+            }
+            
+        }
     }
 }
