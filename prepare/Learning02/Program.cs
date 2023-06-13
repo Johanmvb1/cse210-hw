b6141437fbcda190afe0312ade8afb55501d2c30
JohanMV1 <vil22018@byui.edu>
2023-06-13 15:52:54 -0700

    learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        job job1 = new job();
+        job1._jobTitle = "Sofware Engineer";
+        job1._company = "Micron";
+        job1._startYear = 2020;
+        job1._endYear = 2021;
+
+        job1.DisplayJobDetails();
+
+        job Job2 = new job();
+        Job2._jobTitle ="Senior Engineer";
+        Job2._company ="TSMC";
+        Job2._startYear = 2021;
+        Job2._endYear = 2023;
+
+        Job2.DisplayJobDetails();
+
+        Resume Xresume = new Resume();
+        Xresume._name = "Johan Villarreal";
+
+        
+
+
+
+
+
+
+
+
     }
 }
---
+++ prepare/Learning02/resume.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Resume
+{
+    public string _name;
+
+    public List<job> _jobs = new List<job>();
+
+    public void DisplayResumeDetails()
+    {
+        Console.WriteLine($"name: {_name}" );
+        Console.WriteLine("Jobs");
+
+        
+        foreach (job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+
+        
+    }
+}
