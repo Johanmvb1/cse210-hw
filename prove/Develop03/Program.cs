f73f3b1a73872f0b7bfccf2390aa2b4083219aa2
JohanMV1 <vil22018@byui.edu>
2023-06-26 15:47:13 -0700

    prove 03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -2,5 +2,21 @@ using System;
 
 class Program
 {
+ static void Main(string[]args)
+ {
+    string userInput ="";
+    Reference  reference = new Reference("Proverbs",3,5,6);
+    Scripture scripture = new Scripture(reference, "behold, there came wise men from the east to Jerusalem,");
+
+    while (userInput !=  "quit")
+    {
+        Console.WriteLine(scripture.GetDisplayText());
+        Console.WriteLine();
+        Console.WriteLine("Press enter to continue or type 'quit' to finish");
+        userInput = Console.ReadLine();
+        scripture.hideRandomWords();
+    }
+ }
+
  
 }
